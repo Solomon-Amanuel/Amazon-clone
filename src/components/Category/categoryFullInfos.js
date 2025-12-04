495eea2d1975e9931337d52503daa1d609470792
Solomon-Amanuel <solomonamanuel2018@gmail.com>
2025-12-04 17:12:13 +0100

    Update categoryFullInfos.js
    Update categoryFullInfos.js

--- src/components/Category/categoryFullInfos.js
+++ src/components/Category/categoryFullInfos.js
@@ -1,24 +1,26 @@
 export const categoryInfos = [
   {
-    name: "jewelery",
-    title: "Solid Gold Petite",
-    imgLink: "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
-  },
-  {
-    name: "men's clothing",
-    title: "Mens T-Shirts ",
-    imgLink:
-      "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
-  },
-  {
     name: "electronics",
-    title:
-      "Samsung 49-Inch Gaming Monitor",
-    imgLink: "https://fakestoreapi.com/img/81Zt42ioCgL._AC_SX679_.jpg",
+    title: "Electronics",
+    imgLink:
+      "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Dashboard/Fuji_Dash_TV_2X._SY304_CB432517900_.jpg",
   },
   {
     name: "women's clothing",
-    title: "Rain Jacket Women",
-    imgLink: "https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg",
+    title: "Discover fashion trends",
+    imgLink:
+      "https://images-na.ssl-images-amazon.com/images/G/01/softlines/shopbop/ingress/2023/March/mp_20230219_ff_desktopsinglecategory_desktop_379x304_1._SY304_CB612639047_.jpg",
+  },
+  {
+    name: "men's clothing",
+    title: "Men's clothing",
+    imgLink:
+      "https://m.media-amazon.com/images/I/618bcm65ksL._AC_UL480_FMwebp_QL65_.jpg",
+  },
+  {
+    name: "jewelery",
+    title: "Jewelery",
+    imgLink:
+      "https://m.media-amazon.com/images/I/71r7eWuCsaL._AC_UL480_FMwebp_QL65_.jpg",
   },
 ];
